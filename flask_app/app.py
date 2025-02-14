bd5093712951a7c33da4727d78f8630cf62c2718
Melow <melowfx@proton.me>
2025-02-14 10:24:35 +0200

    upd

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-walkers = [
-    {'name': 'Anna', 'location': 'Viljandi', 'availability': '10 AM - 5 PM'},
-    {'name': 'Mark', 'location': 'Tartu', 'availability': '8 AM - 2 PM'}
+gamers = [
+    {'name': 'Alex', 'game': 'Valorant', 'availability': '6 PM - 10 PM'},
+    {'name': 'Chris', 'game': 'League of Legends', 'availability': '3 PM - 8 PM'},
+    {'name': 'Jordan', 'game': 'CS2', 'availability': '7 PM - 12 AM'},
+    {'name': 'Sam', 'game': 'Valorant', 'availability': '5 PM - 9 PM'}
 ]
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    random_gamer = random.choice(gamers)  # Pick a random gamer for suggestion
+    return render_template('home.html', random_gamer=random_gamer)
 
-@app.route('/dog-walkers')
-def dog_walkers():
-    return render_template('dog_walkers.html', walkers=walkers)
+@app.route('/gamers', methods=['GET'])
+def gamers_list():
+    selected_game = request.args.get('game')  # Get filter from URL parameter
+    filtered_gamers = [g for g in gamers if g['game'] == selected_game] if selected_game else gamers
+    return render_template('gamers.html', gamers=filtered_gamers, selected_game=selected_game)
 
 if __name__ == '__main__':
     app.run(debug=True)
